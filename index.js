d465e6ad89f7ae0d82b19ab553ee0e4e09db4d1a
Dmytro <ryliov.work@gmail.com>
2022-12-13 15:00:46 +0100

    chore: updated file content

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const currentDir = process.cwd();
 const projectName = process.argv[2];
 
 //general checks
-if (projectName === undefined){
+if (projectName === undefined) {
   console.log(chalk.red('Please enter correct folder name'));
   process.exit()
-} 
+}
 
 let projectDir = path.resolve(currentDir, projectName);
 
@@ -23,20 +23,20 @@ if (fs.existsSync(projectDir)) {
   main()
   console.clear()
   console.log(chalk.green('Successfully! Your new project is ready!'));
-  console.log('Created',chalk.bold.yellow(projectName),'at',chalk.bold.yellow(projectDir));
-  console.log('Enter -',chalk.bold.blue(`cd ${projectName},`), 'after -',chalk.bold.blue('npm start'));
+  console.log('Created', chalk.bold.yellow(projectName), 'at', chalk.bold.yellow(projectDir));
+  console.log('Enter -', chalk.bold.blue(`cd ${projectName},`), 'after -', chalk.bold.blue('npm start'));
 }
 
 function main() {
   //create a project directory with the project name
-  fs.mkdirSync(projectDir, { recursive: true });
+  fs.mkdirSync(projectDir, {recursive: true});
   const srcPath = path.resolve(projectDir, "src");
 
   //----------- CREATING FOLDER AND FILE STRUCTURE -----------
   //function for creating folder structure
   const createFolderStructure = (paths) => {
     for (let i = 0; i < paths.length; i++) {
-      fs.mkdirSync(paths[i], { recursive: true });
+      fs.mkdirSync(paths[i], {recursive: true});
     }
   };
 
@@ -197,10 +197,11 @@ function main() {
     {
       path: `${projectDir}/.eslintignore`,
       data: `node_modules
-  .vscode
-  .git
-  .idea
-  `
+dist
+.vscode
+.git
+.idea
+`
     },
     {
       path: `${projectDir}/.eslintrc`,
@@ -235,18 +236,21 @@ function main() {
     },
     {
       path: `${projectDir}/.gitignore`,
-      data: `dist
-  node_modules
-  .idea
-  `
+      data: `node_modules
+dist
+.vscode
+.git
+.idea
+`
     },
     {
       path: `${projectDir}/.prettierignore`,
-      data: `dist
-  node_modules
-  .idea
-  .git
-  `
+      data: `node_modules
+dist
+.vscode
+.git
+.idea
+`
     },
     {
       path: `${projectDir}/.prettierrc.json`,
@@ -267,70 +271,70 @@ function main() {
     {
       path: `${projectDir}/package.json`,
       data: `{
-    "name": "webpack-build",
-    "version": "1.0.0",
-    "description": "yawuxi\`s webpack build for react project",
-    "main": "index.tsx",
-    "scripts": {
-      "start": "webpack-dev-server --node-env development --open",
-      "dev": "webpack --node-env development",
-      "build": "webpack --node-env production"
-    },
-    "keywords": [
-      "webpack",
-      "yawuxi",
-      "react"
-    ],
-    "author": "Dmytro <ryliov.work@gmail.com>",
-    "license": "ISC",
-    "dependencies": {
-      "@reduxjs/toolkit": "^1.9.0",
-      "react": "^18.2.0",
-      "react-dom": "^18.2.0",
-      "react-redux": "^8.0.5"
-    },
-    "devDependencies": {
-      "@babel/cli": "^7.19.3",
-      "@babel/core": "^7.20.2",
-      "@babel/preset-env": "^7.20.2",
-      "@babel/preset-react": "^7.18.6",
-      "@babel/preset-typescript": "^7.18.6",
-      "@types/react": "^18.0.25",
-      "@types/react-dom": "^18.0.8",
-      "@typescript-eslint/eslint-plugin": "^5.42.1",
-      "@typescript-eslint/parser": "^5.42.1",
-      "babel-loader": "^9.1.0",
-      "clean-webpack-plugin": "^4.0.0",
-      "copy-webpack-plugin": "^11.0.0",
-      "core-js": "^3.26.0",
-      "css-loader": "^6.7.1",
-      "css-minimizer-webpack-plugin": "^4.2.2",
-      "eslint": "^8.27.0",
-      "eslint-config-prettier": "^8.5.0",
-      "eslint-config-react-app": "^7.0.1",
-      "eslint-plugin-react": "^7.31.11",
-      "eslint-plugin-react-hooks": "^4.6.0",
-      "eslint-webpack-plugin": "^3.2.0",
-      "file-loader": "^6.2.0",
-      "html-minimizer-webpack-plugin": "^4.3.0",
-      "html-webpack-plugin": "^5.5.0",
-      "prettier": "2.8.1",
-      "sass": "^1.56.1",
-      "sass-loader": "^13.2.0",
-      "style-loader": "^3.3.1",
-      "terser-webpack-plugin": "^5.3.6",
-      "ts-loader": "^9.4.1",
-      "typescript": "^4.8.4",
-      "webpack": "^5.75.0",
-      "webpack-cli": "^4.10.0",
-      "webpack-dev-server": "^4.11.1"
-    },
-    "browserslist": [
-      "last 2 version",
-      ">1%",
-      "not dead"
-    ]
-  }
+  "name": "webpack-build",
+  "version": "1.0.0",
+  "description": "yawuxi\`s webpack build for react project",
+  "main": "index.tsx",
+  "scripts": {
+    "start": "webpack-dev-server --node-env development --open",
+    "dev": "webpack --node-env development",
+    "build": "webpack --node-env production"
+  },
+  "keywords": [
+    "webpack",
+    "yawuxi",
+    "react"
+  ],
+  "author": "Dmytro <ryliov.work@gmail.com>",
+  "license": "ISC",
+  "dependencies": {
+    "@reduxjs/toolkit": "^1.9.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-redux": "^8.0.5"
+  },
+  "devDependencies": {
+    "@babel/cli": "^7.19.3",
+    "@babel/core": "^7.20.2",
+    "@babel/preset-env": "^7.20.2",
+    "@babel/preset-react": "^7.18.6",
+    "@babel/preset-typescript": "^7.18.6",
+    "@types/react": "^18.0.25",
+    "@types/react-dom": "^18.0.8",
+    "@typescript-eslint/eslint-plugin": "^5.42.1",
+    "@typescript-eslint/parser": "^5.42.1",
+    "babel-loader": "^9.1.0",
+    "clean-webpack-plugin": "^4.0.0",
+    "copy-webpack-plugin": "^11.0.0",
+    "core-js": "^3.26.0",
+    "css-loader": "^6.7.1",
+    "css-minimizer-webpack-plugin": "^4.2.2",
+    "eslint": "^8.27.0",
+    "eslint-config-prettier": "^8.5.0",
+    "eslint-config-react-app": "^7.0.1",
+    "eslint-plugin-react": "^7.31.11",
+    "eslint-plugin-react-hooks": "^4.6.0",
+    "eslint-webpack-plugin": "^3.2.0",
+    "file-loader": "^6.2.0",
+    "html-minimizer-webpack-plugin": "^4.3.0",
+    "html-webpack-plugin": "^5.5.0",
+    "prettier": "2.8.1",
+    "sass": "^1.56.1",
+    "sass-loader": "^13.2.0",
+    "style-loader": "^3.3.1",
+    "terser-webpack-plugin": "^5.3.6",
+    "ts-loader": "^9.4.1",
+    "typescript": "^4.8.4",
+    "webpack": "^5.75.0",
+    "webpack-cli": "^4.10.0",
+    "webpack-dev-server": "^4.11.1"
+  },
+  "browserslist": [
+    "last 2 version",
+    ">1%",
+    "not dead"
+  ]
+}
   `,
     },
     {
@@ -367,155 +371,170 @@ function main() {
     {
       path: `${projectDir}/webpack.config.js`,
       data: `const path = require("path");
-  const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-  const HtmlWebpackPlugin = require("html-webpack-plugin");
-  const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
-  const HtmlMinimizerWebpackPlugin = require("html-minimizer-webpack-plugin");
-  const CopyWebpackPlugin = require("copy-webpack-plugin");
-  const TerserWebpackPlugin = require("terser-webpack-plugin");
-  const ESLintWebpackPlugin = require("eslint-webpack-plugin");
-  
-  //getting mode, development or production
-  const mode = process.env.NODE_ENV === "development";
-  
-  module.exports = {
-    //file i/o settings
-    entry: path.resolve(__dirname, "src/index.tsx"),
-    output: {
-      filename: "bundle.[hash].js",
-      path: path.resolve(__dirname, "dist"),
-      publicPath: "/",
-    },
-    resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss", ".sass"],
-    },
-  
-    //webpack settings
-    mode: mode ? "development" : "production",
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserWebpackPlugin({
-          exclude: /node_modules/,
-          include: path.resolve(__dirname, "src"),
-          parallel: true,
-        }),
-        new CssMinimizerWebpackPlugin(),
-      ],
-    },
-    devtool: mode ? "eval-source-map" : "nosources-source-map",
-  
-    //dev server
-    devServer: {
-      port: 4250,
-      historyApiFallback: true,
-    },
-  
-    //loaders
-    module: {
-      rules: [
-        //scss loaders
-        {
-          test: /.s[ac]ss/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-  
-        //js + jsx loaders
-        {
-          test: /.js$/i,
-          exclude: /node_modules/,
-          include: path.resolve(__dirname, "src"),
-          use: "babel-loader",
-        },
-        {
-          test: /.jsx$/i,
-          exclude: /node_modules/,
-          include: path.resolve(__dirname, "src"),
-          use: "babel-loader",
-        },
-  
-        //ts + tsx loaders
-        {
-          test: /.ts$/i,
-          exclude: /node_modules/,
-          include: path.resolve(__dirname, "src"),
-          use: "babel-loader",
-        },
-        {
-          test: /.tsx$/i,
-          exclude: /node_modules/,
-          include: path.resolve(__dirname, "src"),
-          use: "babel-loader",
-        },
-  
-        //image loader
-        {
-          test: /\\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: "file-loader",
-            },
-          ],
-        },
-      ],
-    },
-  
-    // plugins
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin(
-        Object.assign(
-          {},
-          {
-            inject: true,
-            template: path.resolve(__dirname, "src/index.html"),
-          },
-  
-          //Only for production
-          mode
-            ? undefined
-            : {
-                minify: {
-                  removeComments: true,
-                  collapseWhitespace: true,
-                  removeRedundantAttributes: true,
-                  useShortDoctype: true,
-                  removeEmptyAttributes: true,
-                  removeStyleLinkTypeAttributes: true,
-                  keepClosingSlash: true,
-                  minifyJS: true,
-                  minifyCSS: true,
-                  minifyURLs: true,
-                },
-              },
-        ),
-      ),
-      new CssMinimizerWebpackPlugin({
-        minimizerOptions: {
-          presets: [
-            "default",
-            {
-              discardComments: {
-                removeAll: true,
-              },
-            },
-          ],
-        },
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const HtmlMinimizerWebpackPlugin = require("html-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+
+//getting mode, development or production
+const mode = process.env.NODE_ENV === "development";
+
+module.exports = {
+  //file i/o settings
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss", ".sass"],
+  },
+
+  //webpack settings
+  mode: mode ? "development" : "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
+        parallel: true,
       }),
-      new CopyWebpackPlugin({
-        patterns: [
+      new CssMinimizerWebpackPlugin(),
+    ],
+  },
+  devtool: mode ? "eval-source-map" : "nosources-source-map",
+
+  //dev server
+  devServer: {
+    port: 4250,
+    historyApiFallback: true,
+  },
+
+  //loaders
+  module: {
+    rules: [
+      //css loaders
+      {
+        test: /\\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      //scss loaders
+      {
+        test: /\\.s[ac]ss$/i,
+        use: [
+          "style-loader",
           {
-            from: path.resolve(__dirname, "src/assets"),
-            to: path.resolve(__dirname, "dist/assets"),
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]--[hash:base64:7]",
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      //js + jsx loaders
+      {
+        test: /\\.js$/i,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
+      },
+      {
+        test: /\\.jsx$/i,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
+      },
+
+      //ts + tsx loaders
+      {
+        test: /\\.ts$/i,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
+      },
+      {
+        test: /\\.tsx$/i,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
+      },
+
+      //image loader
+      {
+        test: /\\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
           },
         ],
-      }),
-      new HtmlMinimizerWebpackPlugin(),
-      new ESLintWebpackPlugin({
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      }),
+      },
     ],
-  };
+  },
+
+  // plugins
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: path.resolve(__dirname, "src/index.html"),
+        },
+
+        //Only for production
+        mode
+          ? undefined
+          : {
+            minify: {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              keepClosingSlash: true,
+              minifyJS: true,
+              minifyCSS: true,
+              minifyURLs: true,
+            },
+          },
+      ),
+    ),
+    new CssMinimizerWebpackPlugin({
+      minimizerOptions: {
+        presets: [
+          "default",
+          {
+            discardComments: {
+              removeAll: true,
+            },
+          },
+        ],
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+      ],
+    }),
+    new HtmlMinimizerWebpackPlugin(),
+    new ESLintWebpackPlugin({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+  ],
+};
   `,
     },
     //in src folder
@@ -566,7 +585,7 @@ function main() {
       data: ''
     },
     {
-      path: `${srcPath}/scss/css-reset.scss`,
+      path: `${srcPath}/scss/reset.module.scss`,
       data: `* {
     margin: 0;
     padding: 0;
@@ -621,26 +640,24 @@ function main() {
   `
     },
     {
-      path: `${srcPath}/App.scss`,
+      path: `${srcPath}/App.module.scss`,
       data: ''
     },
     {
       path: `${srcPath}/App.tsx`,
-      data: `//react
-  import React from 'react';
-  //styles
-  import './App.scss'
-  
-  const App: React.FC = () => {
-    return (
-      <div className="app">
-        <h1>I am webpack built by yawuxi!</h1>
-      </div>
-    );
-  };
-  
-  export { App };
-  `
+      data: `//styles
+import style from './App.module.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className={style.app}>
+      <h1>I am webpack built by yawuxi!</h1>
+    </div>
+  );
+};
+
+export { App };
+`
     },
     {
       path: `${srcPath}/index.html`,
@@ -660,32 +677,42 @@ function main() {
   `
     },
     {
-      path: `${srcPath}/index.scss`,
-      data: `@import "scss/css-reset";
+      path: `${srcPath}/index.module.scss`,
+      data: `@import "scss/reset.module";
       `
     },
     {
       path: `${srcPath}/index.tsx`,
       data: `//react
-  import React from 'react'
-  import ReactDOM from 'react-dom/client'
-  //rtk
-  import { Provider } from "react-redux";
-  import { store } from "./app/store";
-  //components
-  import { App } from './App'
-  //styles
-  import './index.scss'
-  
-  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  )
-  `
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+//rtk
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+//components
+import { App } from './App'
+//styles
+import './index.module.scss'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+)
+`
+    },
+    {
+      path: `${srcPath}/global.d.ts`,
+      data: `// modules
+declare module '*.module.scss'
+declare module '*.module.css'
+//images
+declare module '*.png'
+declare module '*.jpeg'
+`
     },
   ];
 
